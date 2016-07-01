6c744a46b047cdf6e2d33d28e31dd724b98439ee
Sandeep Vedam <vedamsandeepkumar@gmail.com>
2016-07-02 00:43:40 +0530

    Showing git repositories on username

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,33 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {GitHubService} from '../../services/githubService'
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
+  providers: [GitHubService]
 })
 export class HomePage {
-  constructor(private navController: NavController) {
 
+  public username;
+  public reposData;
+    public isSuccess;
+
+  constructor(private navController: NavController,private gitService : GitHubService) {
+      this.username = '';
+      this.reposData = []
+  }
+
+  private getRepos(){
+    this.gitService.getGitRepos(this.username).subscribe(
+        data =>{
+          this.reposData = data.json();
+            this.isSuccess = true;
+        },
+        err =>{
+          console.error(err);
+            this.isSuccess = false;
+        },
+        () => console.log("Git repos completed.")
+    );
   }
 }
